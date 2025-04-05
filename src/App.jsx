bbb29d03c04d5288541a5f9003ffa440e2ea594d
Qunainb <waniqunain@gmail.com>
2025-04-05 13:57:09 +0530

    First component and some state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
-function App() {}
+import Header from "./Components/Header";
+import Quiz from "./Components/Quiz";
+
+function App() {
+  return (
+    <>
+      <Header />;
+      <Quiz />
+    </>
+  );
+}
 
 export default App;
